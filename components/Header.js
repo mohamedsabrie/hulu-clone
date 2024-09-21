02a3c5eab9f9ea8cb428725bbf8922b96f8986e0
Mohamed Sabry <abosaadmohamed@gmail.com>
2024-09-21 21:57:31 +0700

    change the app logo

--- components/Header.js
+++ components/Header.js
@@ -4,12 +4,7 @@ import Nav from "./Nav";
 function Header() {
   return (
     <header className="flex flex-col lg:flex-row m-5 items-center justify-between px-2 sm:px-10 ">
-      <Image
-        className="object-contain"
-        src="/images/hulu_logo.png"
-        height={100}
-        width={200}
-      />
+      <h1 className="text-4xl font-bold my-5">TrailerFlix</h1>
       <Nav />
     </header>
   );
